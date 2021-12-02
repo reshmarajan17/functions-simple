107090619575cb05edb5d43b64202d29d6750994
reshmarajan17 <82713855+reshmarajan17@users.noreply.github.com>
2021-12-02 17:06:48 +0530

    updated for demo

--- hello/index.js
+++ hello/index.js
@@ -4,6 +4,6 @@ module.exports = async function(context, req) {
   context.res = {
     // status: 200, /* Defaults to 200 */
     body:
-      "Task for Volocopter! by Reshma"
+      "Task for Volocopter! by Reshma demo"
   };
 };
